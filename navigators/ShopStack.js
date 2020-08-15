7f312a4ec94da21ddc71d612322d187e80354e2f
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-08-15 16:04:16 +0200

    Add Some Styling to ShopStack Navigator Header

--- navigators/ShopStack.js
+++ navigators/ShopStack.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import { Platform } from 'react-native'
 import { createStackNavigator } from '@react-navigation/stack'
 
 import ProductsOverviewScreen from '../screens/shop/ProductsOverview'
+import { Colors } from '../theme'
 
 const Stack = createStackNavigator()
 
 const ShopStack = props => {
-    return <Stack.Navigator>
+    return <Stack.Navigator
+        screenOptions={{
+            headerStyle: {
+                backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
+            },
+            headerTintColor: Platform.OS === 'ios' ? Colors.primary : 'white'
+        }}
+    >
         <Stack.Screen
             name='Products'
             component={ProductsOverviewScreen}

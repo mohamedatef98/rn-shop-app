7348caefceaf86cbcc2bf49e3bbb4976237e9830
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-08-15 16:35:50 +0200

    Add ProductItem Component

---
+++ components/ProductItem.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import { Text, View, Image, StyleSheet, Button } from 'react-native'
+import { Colors } from '../theme'
+
+const ProductItem = ({ product, onViewDetails, onToCart }) => {
+    return <View style={styles.productContainer}>
+        <View style={styles.imageContainer}>
+            <Image source={{ uri: product.imageUrl }} style={styles.image} />
+        </View>
+        <View style={styles.details}>
+            <Text style={styles.title}>{product.title}</Text>
+            <Text style={styles.price}>${product.price.toFixed(2)}</Text>
+        </View>
+        <View style={styles.buttonsContainer}>
+            <Button title='View Details' onPress={onViewDetails} color={Colors.primary} />
+            <Button title='To Cart' onPress={onToCart} color={Colors.primary} />
+        </View>
+    </View>
+}
+
+const styles = StyleSheet.create({
+    productContainer: {
+        height: 300,
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 10,
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.26,
+        shadowRadius: 8,
+        elevation: 5,
+        paddingBottom: 10
+    },
+    imageContainer: {
+        height: '60%',
+        overflow: 'hidden',
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+    },
+    image: {
+        width: '100%',
+        height: '100%'
+    },
+    details: {
+        alignItems: 'center',
+        height: '15%'
+    },
+    title: {
+        fontSize: 18,
+        marginVertical: 5
+    },
+    price: {
+        fontSize: 14,
+        color: '#888'
+    },
+    buttonsContainer: {
+        height: '25%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingHorizontal: 20
+    }
+})
+
+export default ProductItem
--- screens/shop/ProductsOverview.js
+++ screens/shop/ProductsOverview.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { FlatList, Text } from 'react-native'
 import { useSelector } from 'react-redux'
 
+import { ProductItem } from '../../components'
+
 const ProductsOverview = (props) => {
     const products = useSelector(state => state.products.availableProducts)
 
-    return <FlatList data={products} renderItem={({ item }) => <Text>{item.title}</Text>} keyExtractor={item => item.id} />
+    return <FlatList data={products} renderItem={({ item }) => <ProductItem product={item} />} keyExtractor={item => item.id} />
 }
 
 export default ProductsOverview

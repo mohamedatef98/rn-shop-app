cc8918e44eb630d724ba2d2ea403ff24a6f00f7d
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-08-17 07:53:52 +0200

    Add Styling to ProductDetails Screen

--- screens/shop/ProductDetails.js
+++ screens/shop/ProductDetails.js
@@ -1,11 +1,41 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { ScrollView, View, Text, Image, StyleSheet, Button } from 'react-native'
 
-const ProductDetails = ({ route }) => {
-    const product = route?.params?.product
-    return <View>
-        <Text>{product.title}</Text>
-    </View>
+const ProductDetails = ({ route, onAddToCart }) => {
+    const product = route?.params?.product || {}
+
+    return <ScrollView>
+        <Image source={{ uri: product.imageUrl }} style={styles.image} />
+        <View style={styles.buttonContainer}>
+            <Button onPress={onAddToCart} title='Add to Cart' />
+        </View>
+        <Text style={[styles.title, styles.textCenter]}>{product.title}</Text>
+        <Text style={[styles.description, styles.textCenter]}>{product.description}</Text>
+    </ScrollView>
 }
+
+const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: 200
+    },
+    buttonContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginVertical: 10
+    },
+    textCenter: {
+        textAlign: 'center'
+    },
+    title: {
+        color: '#888',
+        fontSize: 20,
+        marginVertical: 20
+    },
+    description: {
+        fontSize: 14,
+        marginHorizontal: 20
+    }
+})
 
 export default ProductDetails

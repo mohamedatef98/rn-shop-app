a41e0898be51860d78e4fae79fe39c8158e9e423
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-08-17 08:04:05 +0200

    Load our custom fonts in App.js

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Provider } from 'react-redux'
 import { NavigationContainer } from '@react-navigation/native'
+import { AppLoading } from 'expo'
+import { loadAsync } from 'expo-font'
 
+import { FontPaths } from './theme'
 import ShopStack from './navigators/ShopStack'
 
 import store from './store'
 
+const loadFonts = () => loadAsync(FontPaths)
+const startAsync = () => Promise.all[loadFonts()]
+
 export default function App() {
+    const [isLoading, setIsLoading] = useState(true)
+    if (isLoading) return <AppLoading
+        startAsync={startAsync}
+        onFinish={() => setIsLoading(false)}
+        onError={console.err}
+    />
     return (
         <Provider store={store}>
             <NavigationContainer>

7c582b6e1f8bbb49c20ad348b910e01eafb880eb
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-08-26 14:14:39 +0200

    Add Add to Cart Actions to Buttons

--- screens/shop/ProductDetails.js
+++ screens/shop/ProductDetails.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { ScrollView, View, Image, StyleSheet, Button } from 'react-native'
-import { Text } from '../../components'
+import { useDispatch } from 'react-redux'
 
-const ProductDetails = ({ route, onAddToCart }) => {
+import { Text } from '../../components'
+import { actions } from '../../store'
+
+const ProductDetails = ({ route }) => {
     const product = route?.params?.product || {}
+    const dispatch = useDispatch()
 
     return <ScrollView>
         <Image source={{ uri: product.imageUrl }} style={styles.image} />
         <View style={styles.buttonContainer}>
-            <Button onPress={onAddToCart} title='Add to Cart' />
+            <Button onPress={() => dispatch(actions.addToCart(product))} title='Add to Cart' />
         </View>
         <Text style={[styles.title, styles.textCenter]} mode='bold'>{product.title}</Text>
         <Text style={[styles.description, styles.textCenter]}>{product.description}</Text>
--- screens/shop/ProductsOverview.js
+++ screens/shop/ProductsOverview.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import { FlatList, Text } from 'react-native'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
 import { ProductItem } from '../../components'
+import { actions } from '../../store/cart/actions'
 
 const ProductsOverview = ({ navigation }) => {
     const products = useSelector(state => state.products.availableProducts)
+    const dispatch = useDispatch()
 
     return <FlatList
         data={products}
         renderItem={({ item }) => <ProductItem
             product={item}
             onViewDetails={() => navigation.navigate('ProductDetails', { product: item })}
+            onToCart={() => dispatch(actions.addToCart(item))}
         />}
         keyExtractor={item => item.id}
     />

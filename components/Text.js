9346efb0853f9fa13249d5fb7cb8b7476943c7c8
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-08-26 15:05:45 +0200

    Remove mode prop From Text

--- components/Text.js
+++ components/Text.js
@@ -3,8 +3,8 @@ import { Text as RNText } from 'react-native'
 
 import { Fonts } from '../theme'
 
-const Text = ({ children, style, mode='regular',  ...rest }) => {
-    const fontFamily = mode === 'bold' ? Fonts.bold : Fonts.primary
+const Text = ({ children, style, bold = false,  ...rest }) => {
+    const fontFamily = bold ? Fonts.bold : Fonts.primary
     return <RNText style={[{ fontFamily }, style]} {...rest}>{children}</RNText>
 }
 

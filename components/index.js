bb6a2052603123755032aaeca56465b15585b5cd
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-08-26 15:43:26 +0200

    Display Cart Items in cart Screen

--- components/index.js
+++ components/index.js
@@ -1,3 +1,4 @@
 export { default as ProductItem } from './ProductItem'
 export { default as Text } from './Text'
 export { default as HeaderButton } from './HeaderButton'
+export { default as CartItem } from './CartItems'
--- screens/shop/Cart.js
+++ screens/shop/Cart.js
@@ -1,17 +1,67 @@
 import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { View, StyleSheet, Button, FlatList } from 'react-native'
+import { useSelector } from 'react-redux'
+
+import { Text, CartItem } from '../../components'
+import { Colors } from '../../theme'
 
 const Cart = props => {
-    return <View>
-        <View></View>
-        <View>
-            <Text>Cart Items:</Text>
+    const cart = useSelector(state => state.cart)
+    const cartItems = Object.entries(cart.items).map(([id, product]) => ({
+        id,
+        title: product.title,
+        price: product.price,
+        qty: product.qty,
+        sum: product.sum
+    }))
+    console.log(cartItems)
+    return <View style={styles.screen}>
+        <View style={styles.orderSummary}>
+            <Text bold style={styles.total}>Total: {' '}
+                <Text style={styles.totalNumber}>${cart.totalAmount.toFixed(2)}</Text>
+
+            </Text>
+            <Button title='Order now' color={Colors.accent} />
         </View>
+        <FlatList
+            data={cartItems}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => (
+                <CartItem
+                    title={item.title}
+                    qty={item.qty}
+                    sum={item.sum}
+                    onRemove={() => { }}
+                />
+            )}
+        />
     </View>
 }
 
 const styles = StyleSheet.create({
-    
+    screen: {
+        margin: 20
+    },
+    orderSummary: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        shadowColor: 'black',
+        shadowOpacity: 0.26,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 8,
+        elevation: 5,
+        borderRadius: 10,
+        padding: 10,
+        marginBottom: 20
+    },
+    total: {
+        fontSize: 18
+    },
+    totalNumber: {
+        color: Colors.primary
+    }
 })
 
 export default Cart

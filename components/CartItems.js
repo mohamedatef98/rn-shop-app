ccd84b415488709e31dc3808cd8824e508500557
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-08-26 15:43:11 +0200

    Add CartItem Component

---
+++ components/CartItems.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { View, TouchableOpacity, StyleSheet } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
+
+import Text from './Text'
+
+
+const CartItem = ({ title, qty, sum, onRemove }) => {
+    return <View style={styles.item}>
+        <View style={styles.container}>
+            <Text style={styles.qty}>{qty} </Text>
+            <Text bold>{title}</Text>
+        </View>
+        <View style={styles.container}>
+            <Text bold>{sum} </Text>
+            <TouchableOpacity onPress={onRemove} style={styles.delete}>
+                <Ionicons name='ios-trash' color='red' size={23} />
+            </TouchableOpacity>
+        </View>
+    </View>
+}
+
+const styles = StyleSheet.create({
+    item: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 10,
+    },
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    qty: {
+        fontSize: 16,
+        color: '#888'
+    },
+    delete: {
+        paddingLeft: 20
+    }
+})
+
+export default CartItem

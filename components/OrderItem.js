671ae7b5062ad1b7eab6bdce96fe7930df1b5ff3
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-10-22 01:27:39 +0200

    Add OrderItem Component

---
+++ components/OrderItem.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { View, StyleSheet, Button, Text } from 'react-native'
+
+import { Colors, Fonts } from '../theme'
+
+const getFormattedOrderDate = date => date.toDateString()
+
+export default function OrderItem({ order }) {
+    return <View style={styles.orderItem}>
+        <View style={styles.summary}>
+            <Text style={styles.totalAmount}>${Math.abs(order.totalAmount).toFixed(2)}</Text>
+            <Text style={styles.date}>{getFormattedOrderDate(order.date)}</Text>
+        </View>
+        <Button color={Colors.primary} title='Show Details' />
+    </View>
+}
+
+const styles = StyleSheet.create({
+    orderItem: {
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 10,
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.26,
+        shadowRadius: 8,
+        elevation: 5,
+        padding: 10,
+        alignItems: 'center'
+    },
+    summary: {
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 10
+    },
+    totalAmount: {
+        fontFamily: Fonts.primary
+    },
+    date: {
+        fontFamily: Fonts.primary,
+        color: 'grey'
+    }
+})
--- screens/orders/Orders.js
+++ screens/orders/Orders.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { FlatList, Text } from 'react-native'
 import { useSelector } from 'react-redux'
 
+import OrderItem from '../../components/OrderItem'
+
 export default function Orders() {
     const orders = useSelector(s => s.orders.orders)
 
-    console.log(orders)
-
     return <FlatList
         data={orders}
-        renderItem={({ item }) => <Text>{item.totalAmount}</Text>}
+        renderItem={({ item }) => <OrderItem order={item} />}
     />
 }

0c1c451594eea0ed46237ebcd6cd887c139101da
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-10-22 16:10:40 +0200

    Add AddProduct Screen to AdminStack Navigator

--- navigators/AdminStack.js
+++ navigators/AdminStack.js
@@ -2,7 +2,10 @@ import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 
 import UserProductScreen from '../screens/users/UserProducts'
+import AddProductScreen from '../screens/users/AddProduct'
 import sharedOptions from './sharedStackOptions'
+
+import HeaderButton from '../components/HeaderButton'
 
 const Stack = createStackNavigator()
 
@@ -15,7 +18,20 @@ const AdminStack = props => {
         <Stack.Screen
             name='UserProducts'
             component={UserProductScreen}
-            options={{ title: 'Your Products' }}
+            options={({ navigation }) => ({
+                title: 'Your Products',
+                headerRight: ({ tintColor }) => <HeaderButton
+                    color={tintColor}
+                    size={30}
+                    iconName={Platform.OS === 'android' ? 'md-create' : 'ios-create'}
+                    onPress={() => navigation.navigate('AddProduct')}
+                />
+            })}
+        />
+        <Stack.Screen
+            name='AddProduct'
+            component={AddProductScreen}
+            options={{ title: 'Add Product' }}
         />
     </Stack.Navigator>
 }

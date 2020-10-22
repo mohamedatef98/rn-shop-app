e3699030a97b9b5d6ed9ef0f93959e55383e90af
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-10-22 16:06:49 +0200

    Add AddProduct Screen

---
+++ screens/users/AddProduct.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function AddProduct() {
+    return <View>
+        <Text>Add Product Screen!</Text>
+    </View>
+}

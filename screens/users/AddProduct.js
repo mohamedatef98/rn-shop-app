1f9dc1373900ba18fde0ff55b44f429dfc28be55
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-10-27 00:55:38 +0200

    Add Form Validation to Add&EditProduct Forms

--- screens/users/AddProduct.js
+++ screens/users/AddProduct.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useCallback, useContext, useEffect, useState } from 'react'
 import { Alert, View } from 'react-native'
 import { useDispatch } from 'react-redux'
 
@@ -11,25 +11,38 @@ const emptyProduct = new Product('', '', '', '', '', 0)
 
 export default function AddProduct({ navigation }) {
     const [newProduct, setNewProduct] = useState(emptyProduct)
+    const [formErrors, setFormErrors] = useState({})
     const { setOnCreateProduct } = useContext(ProductFormSubmitContext)
     const dispatch = useDispatch()
 
     useEffect(() => {
         setOnCreateProduct(
             () => () => {
-                dispatch(actions.addProduct({
-                    title: newProduct.title,
-                    imageUrl: newProduct.imageUrl,
-                    price: +newProduct.price,
-                    description: newProduct.description
-                }))
-                Alert.alert('Done!', 'Product Added', [{ text: 'Ok' }])
-                navigation.navigate('UserProducts')
+                const formErrors = {}
+
+                if (!newProduct.title.trim()) formErrors.title = 'Title is required'
+                if (!newProduct.description.trim()) formErrors.description = 'Description is required'
+                if ((+newProduct.price) < 0) formErrors.price = 'Invalid Price'
+                if (!newProduct.imageUrl.trim()) formErrors.imageUrl = 'Image URL is required'
+
+
+                if (Object.keys(formErrors).length) setFormErrors(formErrors)
+                else {
+                    setFormErrors({})
+                    dispatch(actions.addProduct({
+                        title: newProduct.title,
+                        imageUrl: newProduct.imageUrl,
+                        price: +newProduct.price,
+                        description: newProduct.description
+                    }))
+                    Alert.alert('Done!', 'Product Added', [{ text: 'Ok' }])
+                    navigation.navigate('UserProducts')
+                }
             }
         )
     }, [newProduct, navigation])
-    
+
     return <View>
-        <ProductForm product={newProduct} onChange={setNewProduct} priceEditable />
+        <ProductForm product={newProduct} onChange={setNewProduct} priceEditable errors={formErrors} />
     </View>
 }
--- screens/users/EditProduct.js
+++ screens/users/EditProduct.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useCallback, useContext, useEffect, useState } from 'react'
 import { Alert, View } from 'react-native'
 import { useDispatch } from 'react-redux'
 
@@ -8,25 +8,36 @@ import { actions } from '../../store/product/actions'
 
 export default function EditProduct({ navigation, route }) {
     const [newProduct, setNewProduct] = useState(route.params.product)
+    const [formErrors, setFormErrors] = useState({})
     const { setOnEditProduct } = useContext(ProductFormSubmitContext)
     const dispatch = useDispatch()
 
     useEffect(() => {
         setOnEditProduct(
             () => () => {
-                dispatch(actions.editProduct({
-                    id: newProduct.id,
-                    title: newProduct.title,
-                    imageUrl: newProduct.imageUrl,
-                    description: newProduct.description
-                }))
-                Alert.alert('Done!', 'Product Edited', [{ text: 'Ok' }])
-                navigation.navigate('UserProducts')
+                const formErrors = {}
+
+                if (!newProduct.title.trim()) formErrors.title = 'Title is required'
+                if (!newProduct.description.trim()) formErrors.description = 'Description is required'
+                if (!newProduct.imageUrl.trim()) formErrors.imageUrl = 'Image URL is required'
+
+
+                if (Object.keys(formErrors).length) setFormErrors(formErrors)
+                else {
+                    dispatch(actions.editProduct({
+                        id: newProduct.id,
+                        title: newProduct.title,
+                        imageUrl: newProduct.imageUrl,
+                        description: newProduct.description
+                    }))
+                    Alert.alert('Done!', 'Product Edited', [{ text: 'Ok' }])
+                    navigation.navigate('UserProducts')
+                }
             }
         )
     }, [newProduct, navigation])
-    
+
     return <View>
-        <ProductForm product={newProduct} onChange={setNewProduct} />
+        <ProductForm product={newProduct} onChange={setNewProduct} errors={formErrors} />
     </View>
 }

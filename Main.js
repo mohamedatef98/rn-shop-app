e9a33bc0b7e788d77afa0c5b39fd912fec8b0dc1
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-10-30 15:46:23 +0200

    Add fetching products in ProductsOverview Screen

--- Main.js
+++ Main.js
@@ -1,17 +1,9 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
-import { useDispatch } from 'react-redux'
 
 import DrawerNavigator from './navigators/Drawer'
-import { actions } from './store'
 
 const Main = () => {
-    const dispatch = useDispatch()
-
-    useEffect(() => {
-        dispatch(actions.getProducts())
-    }, [])
-
     return <NavigationContainer>
         <DrawerNavigator />
     </NavigationContainer>
--- screens/shop/ProductsOverview.js
+++ screens/shop/ProductsOverview.js
@@ -1,14 +1,50 @@
-import React from 'react'
-import { FlatList, Button } from 'react-native'
+import React, { useCallback, useEffect, useState } from 'react'
+import { FlatList, Button, View, ActivityIndicator, StyleSheet, RefreshControl, Text } from 'react-native'
 import { useSelector, useDispatch } from 'react-redux'
 
 import { ProductItem } from '../../components'
-import { actions } from '../../store/cart/actions'
+import { actions } from '../../store'
 import { Colors } from '../../theme'
+
 
 const ProductsOverview = ({ navigation }) => {
     const products = useSelector(state => state.products.availableProducts)
+    const [fetching, setFetching] = useState(false)
+    const [refreshing, setRefreshing] = useState(false)
+    const [error, setError] = useState(false)
     const dispatch = useDispatch()
+
+    const fetchProducts = useCallback(
+        () => dispatch(actions.getProducts()),
+        []
+    )
+
+    useEffect(() => {
+        setFetching(true)
+        fetchProducts()
+            .catch(() => setError(true))
+            .finally(() => setFetching(false))
+    }, [])
+
+    const handleRefresh = useCallback(
+        () => {
+            setRefreshing(true)
+            setError(false)
+            fetchProducts()
+                .catch(() => setError(true))
+                .finally(() => setRefreshing(false))
+        },
+        []
+    )
+
+    if (fetching) return <View style={styles.centered}>
+        <ActivityIndicator animating size='large' />
+    </View>
+
+    if (error) return <View style={styles.centered}>
+        <Text>An Error Occured!</Text>
+        <Button title='Reload?' color={Colors.primary} onPress={handleRefresh} />
+    </View>
 
     return <FlatList
         data={products}
@@ -28,7 +64,16 @@ const ProductsOverview = ({ navigation }) => {
             />
         </ProductItem>}
         keyExtractor={item => item.id}
+        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={handleRefresh} />}
     />
 }
+
+const styles = StyleSheet.create({
+    centered: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
 
 export default ProductsOverview
